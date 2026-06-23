dfb3ca36c1b93becee81384bbe008d5abc6865b7
ROHIT KUMAR <bicky2005rohit@gmail.com>
2026-06-23 20:07:01 +0530

    chore: remove unused test.js script

--- test.js
+++
@@ -1,4 +0,0 @@
-fetch('https://habitiq.onrender.com/assets/index-vT0kRTOI.js').then(r => r.text()).then(js => {
-    const urls = js.match(/https:\/\/[^\"]+onrender\.com/g) || [];
-    console.log(urls.length ? urls : 'Not found');
-}).catch(console.error);
